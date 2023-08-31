e8f482f836937c7c27b03f4bcc9b23f580d4592f
Debabrata <ghoraideb@gmail.com>
2023-08-31 12:33:17 +0530

    update files

--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
 # import modules
-import sys
+# import sys
+from setuptools import setup, find_packages
 
-class VersionError(Exception):
-    def __init__(self, *args, **kwargs):
-        Exception.__init__(self, *args, **kwargs)
+# class VersionError(Exception):
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    raise("Please install setuptools to run setup.py")
+#     def __init__(self, *args, **kwargs):
+#         Exception.__init__(self, *args, **kwargs)
+#         return
+
 
 def readme():
     with open('README.md') as f:
         return f.read()
 
+
 def get_requirements():
     with open('requirements.txt') as f:
         return f.read().split()
 
+
 setup(
-    name='rsgis-scripts',
-    version='0.1.0',
+    name='rsgis-toolbox',
+    version='0.0.2',
     packages=find_packages(),
-    url='https://github.com/dghorai83',
+    url='https://github.com/dghorai',
     license='MIT License',
     author='Debabrata Ghorai, Ph.D.',
     author_email='ghoraideb@gmail.com',
     description='Geospatial utilities for Remote Sensing and GIS application',
     long_description=readme(),
     install_requires=get_requirements()
-    )
+)
